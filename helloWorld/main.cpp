715c19512eb3b9dd657333236c176c69790f2944
F <saw@Saws-Air.gateway.2wire.net>
2016-07-27 00:13:25 +0800

    3.5.1
    An example: detect repeated words

--- helloWorld/main.cpp
+++ helloWorld/main.cpp
@@ -14,13 +14,11 @@ using namespace std;
 // read first and second name
 
 int main() {
-    cout << "Please enter two names\n";
-    string first;
-    string second;
-    cin >> first >> second; // read two strings
-    if (first == second) cout << "that's the same name twice\n";
-    if (first < second)
-        cout << first << " is alphabetically before " << second << '\n';
-    if (first > second)
-        cout << first << " is alphabetically after " << second << '\n';
+    string previous = " "; // previous word; initialized to "not a word"
+    string current; // current word
+    while (cin >> current) { // read a stream of words
+        if (previous == current) // check if the word is the same as last
+            cout << "repeated word: " << current << '\n';
+        previous = current;
+    }
 }
