710322f76104ca3a1fba0aa2a3e763a8a29ea1de
F <saw@Saws-Air.gateway.2wire.net>
2016-07-27 23:52:38 +0800

    3.9.2
    Unsafe conversions

--- helloWorld/main.cpp
+++ helloWorld/main.cpp
@@ -15,11 +15,15 @@ using namespace std;
 
 int main()
 {
-    int a = 20000;
-    char c = a; // try to squeeze a large int into a small char
-    int b = c;
-    if (a != b) // != means "not equal"
-        cout << "Oops!: " << a << " != " << b << '\n';
-    else
-        cout << "Wow! We have large characters\n";
+    double d = 0;
+    while (cin >> d) { // repeat the statements below
+        // as long as we type in numbers
+        int i = d; // try to squeeze a double into an int
+        char c = i; // try to squeeze an int into a char
+        int i2 = c; // get the integer value of the character
+        cout << "d == " << d << ", " // the original double
+        << "i == " << i << ", " // converted into int
+        << "i2 == " << i2 << ", " // int value of char
+        << "char("<< c <<")\n"; // the char
+    }
 }
