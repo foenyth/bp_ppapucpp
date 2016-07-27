951c86b9b17e9ae5617c408d3db3c2c26f2bb3db
F <saw@Saws-Air.gateway.2wire.net>
2016-07-27 23:40:39 +0800

    3.9.1
    Double comparison.

--- helloWorld/main.cpp
+++ helloWorld/main.cpp
@@ -15,9 +15,8 @@ using namespace std;
 
 int main()
 {
-    char c = 'x';
-    int i1 = c;
-//    int i2 = 'x';
-    char c2 = i1;
-    cout << c << " " << i1 << " " << c2 << '\n';
+    double d1 = 2.3;
+//    double d2 = d1 + 2; // 2 is converted to 2.0 bfore adding
+    if (d1 < 3) // 0 is converted to 0.0 before comparison
+        cout << "d1 is less than half of ten.\n";
 }
