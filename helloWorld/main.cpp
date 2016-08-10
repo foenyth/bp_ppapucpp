52d83a34379576821ee7f354b81e0b6a3aa3fb17
F <saw@Saws-Air.gateway.2wire.net>
2016-08-10 23:31:17 +0800

    C.3 Drill

--- helloWorld/main.cpp
+++ helloWorld/main.cpp
@@ -7,7 +7,8 @@
 //
 
 #include <iostream>
-#include <cmath>
+//#include <cmath>
+//#include "std_lib_facilities.h"
 
 using namespace std;
 
@@ -15,15 +16,42 @@ using namespace std;
 
 int main()
 {
-    double d=0;
-    while (cin>>d) {    // repeat the statments below
-                        // as long as we type in numbers
-        int i = d;      // try to squeeze a double into an int
-        char c = i;     // try to squeeze an int into a char
-        int i2 = c;     // get the integer value of the character
-        cout << "d==" << d  // the original double
-                << " i==" << i   // converted to int
-                << " i2==" << i2 // int value of char
-                << " char("<<c<<")\n";  // the char
-    }
+    cout << "Enter the name of the person you want to write to: ";
+    string first_name; // first_name is a variable of type string
+    cin >> first_name; // read characters into first_name
+    cout << "\nDear " << first_name << ",\n";
+    cout << "How are you?\n";
+    cout << "I am fine. I miss you.\n";
+    
+    cout << "\nPlease enter the first name of your friend: ";
+    string friend_name; // friend_name is a variable of type string
+    cin >> friend_name; // read characters into friend_name
+    cout << "\nHave you seen " << friend_name << " lately?\n";
+    
+    char friend_sex = 0;
+    cout << "\nPlease enter your gender (F/M): ";
+    cin >> friend_sex;
+    
+    if (friend_sex == 'M')
+        cout << "\nIf you see " << friend_name << ", please ask him to call me. Thanks you!\n";
+    else if (friend_sex == 'F')
+        cout << "\nIf you see " << friend_name << ", please ask her to call me. Thanks you!\n";
+    
+    cout << "\nPlease enter your age: ";
+    int age;
+    cin >> age;
+    cout << "\nI hear you just had a birthday and you are " << age << " years old.\n";
+    if (age <= 0 || age >= 110)
+        cout << "You're kidding!\n";
+    else if (age <= 12)
+        cout << "Next year you will be " << age+1 << ".\n";
+    else if (age  == 17)
+        cout << "Next year you will be able to vote.\n";
+    else if (age > 70)
+        cout << "I hope you are enjoying retirement.\n";
+    
+    cout << "\nYours sincerely, \n";
+    cout << "Cpt. S\n\n";
+    
+    return 0;
 }
