85f3818524f0c5e6552812bd4f1cbdb4bfe4990f
F <foenyth@gmail.com>
2016-08-16 20:09:00 +0800

    Chapter 3 Exercise 9a (self)
    Reverse version of Exercise 9, numbers (‘0’) to text (‘zero’).

---
+++ exers/chapter03/3.9a/rspelledout/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  spelledout
+//
+//  Created by F on 16/8/16.
+//  Copyright © 2016 phantom. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    //  Ask user to input one of the provided numbers
+    std::cout << "Please choose one of these numbers: (0, 1, 2, 3 and 4) ";
+    
+    //  Take input
+    int val = 0;
+    std::cin >> val;
+    //    std::cout << "You chose " << val << ".\n";
+    
+    if (val == 0)
+        std::cout << "\nYou chose '0'. It's the zero.\n";
+    else if (val == 1)
+        std::cout << "\nIt's the one.\n";
+    else if (val == 2)
+        std::cout << "\nIt's the two.\n";
+    else if (val == 3)
+        std::cout << "\nIt's the three.\n";
+    else if (val == 4)
+        std::cout << "\nIt's the four.\n";
+    else
+        std::cout << "Come on, " << val << " is not a number I know!\n";
+    
+    return 0;
+}
