9ccb4486f3289cacb3efcd60db59de9bf95e2b26
F <foenyth@gmail.com>
2016-08-24 23:54:00 +0800

    Rewrite with Switch
    Add conversion from krona.

--- exers/chapter04/currency_conversion/currency_conversion/main.cpp
+++ exers/chapter04/currency_conversion/currency_conversion/main.cpp
@@ -13,20 +13,28 @@ int main()
     constexpr double yen_per_dollar = 0.0100;
     constexpr double euro_per_dollar = 1.13;
     constexpr double pound_per_dollar = 1.32;
+    constexpr double krona_per_dollar = 8.42;
     double amount = 1;
-    char unit = 0;
-    std::cout << "Please enter an amount follwed by a unit ('y', 'e', 'p'): ";
+    char unit;
+    std::cout << "Please enter an amount follwed by a unit ('k', 'y', 'e', 'p'): ";
     
     std::cin >> amount >> unit;
     
-    if (unit == 'y')
-        std::cout << amount << "y == " << yen_per_dollar * amount << "d.\n";
-    else if (unit == 'e')
-        std::cout << amount << "e == " << euro_per_dollar * amount << "d.\n";
-    else if (unit == 'p')
-        std::cout << amount << "p == " << pound_per_dollar * amount << "d.\n";
-    else
-        std::cout << "Sorry, we don't know a unit called " << unit << "\n";
-    
-    return 0;
+    switch (unit) {
+        case'k':
+            std::cout << amount << "k == " << krona_per_dollar * amount << "d.\n";
+            break;
+        case'y':
+            std::cout << amount << "y == " << yen_per_dollar * amount << "d.\n";
+            break;
+        case'e':
+            std::cout << amount << "e == " << euro_per_dollar * amount << "d.\n";
+            break;
+        case'p':
+            std::cout << amount << "p == " << pound_per_dollar * amount << "d.\n";
+            break;
+        default:
+            std::cout << "Sorry, we don't know a unit called " << unit << "\n";
+            break;
+    }
 }
